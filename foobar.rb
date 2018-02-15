5ca466aef920814be1fc08c93be9648b970e7fc1
Samir Puranik <samir.puranik@berkeley.edu>
2018-02-14 22:15:30 -0800

    Questions 1, 2, and 3

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,7 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    
+
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,22 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  (a+b)**2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a = a.sort
+	a.map! do |element|
+		element + 1
+	end
+  return a
 end
 
+
+
 def combine_name(first_name, last_name)
-  # Q3 CODE HERE
+  first_name + " " + last_name
 
 end
 
